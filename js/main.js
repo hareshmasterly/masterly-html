18ee585c11d687c0a2328d86149cda5968763788
BirjooDave <birjoo.d@masterlysolutions.com>
2023-11-22 17:29:31 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+
+
+
+    //Back to Top
+    $('.backToTop').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500);
+        return false;
+    });
+
+
+})(jQuery);
+
